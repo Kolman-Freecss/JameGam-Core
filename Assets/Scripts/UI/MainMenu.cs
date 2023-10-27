645708b7c7fd1fb31c5c9597ab673b3a23a0d8c9
AVSienes <a.vicentesienes@gmail.com>
2023-10-27 19:34:50 +0200

    ADD. script main menu UPDATE scripts and panels

---
+++ Assets/Scripts/UI/MainMenu.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using 
+
+public class MainMenu : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void ExitGame()
+    {
+        Application.Quit();
+    }
+}
