e9b93ace7a14009aada7e55a2f3cd7d55e128794
Kolman-Freecss <s3rg10.martinez.roman@gmail.com>
2023-10-27 20:18:11 +0200

    Move AssetPacks to another folder
    WIP PlayerBehaviour
    New InputSystem

--- Assets/Scripts/CameraLimit.cs
+++ Assets/Scripts/CameraLimit.cs
@@ -2,7 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Limit : MonoBehaviour
+public class CameraLimit : MonoBehaviour
 {
     // Start is called before the first frame update
     void Start()
