8b06be44f41d4d2245300469250c2121ce669853
AVSienes <a.vicentesienes@gmail.com>
2023-10-28 17:01:07 +0200

    ADD script places working

--- Assets/Scripts/UI/TriggerLocations.cs
+++ Assets/Scripts/UI/TriggerLocations.cs
@@ -1,9 +1,14 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
 
 public class TriggerLocations : MonoBehaviour
 {
+    [SerializeField] private GameObject [] Triggers;
+    [SerializeField] private TextMeshProUGUI textPlaces;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -14,5 +19,16 @@ public class TriggerLocations : MonoBehaviour
     void Update()
     {
         
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+            Debug.Log("entro al trigger");
+        // Verifica si el objeto que colisionó tiene la etiqueta "Lugar"
+        if (collision.gameObject.layer == LayerMask.NameToLayer("Places"))
+        {
+            // Actualiza el texto con el nombre del lugar
+            textPlaces.text = collision.gameObject.name;
+        }
     }
 }
