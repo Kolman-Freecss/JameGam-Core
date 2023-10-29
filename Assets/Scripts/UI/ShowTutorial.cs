1531364f2ba9876e01d6d6f69c42ddeb0ac51ed2
AVSienes <a.vicentesienes@gmail.com>
2023-10-29 20:03:18 +0100

    UPDATE. trigger tutorial

--- Assets/Scripts/UI/ShowTutorial.cs
+++ Assets/Scripts/UI/ShowTutorial.cs
@@ -6,15 +6,18 @@ using UnityEngine.UI;
 
 public class ShowTutorial : MonoBehaviour
 {
+    [SerializeField] private GameObject canvasTutorial;
+    [SerializeField] private GameObject triggerRoom;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        canvasTutorial.SetActive(true);
     }
 
     // Update is called once per frame
-    void Update()
+    private void OnTriggerExit2D(Collider2D collision)
     {
-        
+        canvasTutorial.SetActive(false);
     }
 }
